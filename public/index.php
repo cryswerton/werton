fa81b97897b3f2a74d266880b84ce0fee34efe04
cryswerton <cryswertons@gmail.com>
2025-02-01 19:43:22 +0000

    refactoring

--- public/index.php
+++ public/index.php
@@ -6,8 +6,6 @@ echo "<br>";
 
 $composerPath = realpath(__DIR__ . '/..') . '/composer.json';
 
-echo $composerPath;
-
 $composer = json_decode(file_get_contents($composerPath));
 
 echo "Version: " . $composer->version;
