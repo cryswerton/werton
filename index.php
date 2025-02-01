1d9aa57ea31b785d119eed9d1420c24d5d12b94f
cryswerton <cryswertons@gmail.com>
2025-02-01 02:06:56 +0000

    it shows the framework version on the index page

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
 echo "WertonPHP says hello!";
+
+echo "<br>";
+
+$composer = json_decode(file_get_contents(__DIR__ . '/composer.json'));
+
+echo "Version: " . $composer->version;
--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -5,7 +5,7 @@ use PHPUnit\Framework\TestCase;
 class IndexTest extends TestCase{
 
 
-    public function testIndexOutputExpectedMessage(){
+    public function testIndexOutputsExpectedMessage(){
 
         ob_start();
 
@@ -14,6 +14,19 @@ class IndexTest extends TestCase{
         $output = ob_get_clean();
 
         $message = 'WertonPHP says hello!';
+
+        $this->assertStringContainsString($message, $output);
+    }
+
+    public function testIndexShowsFrameworkVersion(){
+
+        ob_start();
+
+        include 'index.php';
+
+        $output = ob_get_clean();
+
+        $message = 'Version: 1.0.0';
 
         $this->assertStringContainsString($message, $output);
     }
